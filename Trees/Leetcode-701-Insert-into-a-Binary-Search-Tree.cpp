dc9b673148588641896ada226088b0eeb205b21f
Pranav N <pranavbharadwaj142@gmail.com>
2021-12-17 19:14:06 +0530

    Create Leetcode-701-Insert-into-a-Binary-Search-Tree.cpp

---
+++ Trees/Leetcode-701-Insert-into-a-Binary-Search-Tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL) return new TreeNode(val);
+        TreeNode* curr=root;
+        while(true){
+            if(curr->val<=val){
+                if(curr->right)curr=curr->right;
+                else{
+                    curr->right=new TreeNode(val);
+                    break;
+                }
+            }
+            else{
+                if(curr->left)curr=curr->left;
+                else{
+                    curr->left=new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+};
