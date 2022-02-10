4bfe63d6a237822d6faea18fcaaefd090b9fba6f
Pranav N <pranavbharadwaj142@gmail.com>
2022-02-10 08:50:26 +0530

    Leetcode 189

---
+++ Vector-Array/rotate-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.begin()+n-k);
+        reverse(nums.begin()+n-k,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+};
