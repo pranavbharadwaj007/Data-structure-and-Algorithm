f550320bcd58c8af8fefcc71ee0d5b8ac465d466
Pranav N <pranavbharadwaj142@gmail.com>
2022-03-15 06:58:50 +0530

    L1249 Internal stack + string

--- Strings/minimum-remove-to-make-valid-parentheses.cpp
+++ Strings/minimum-remove-to-make-valid-parentheses.cpp
@@ -2,44 +2,44 @@
 
 class Solution {
 public:
-	string minRemoveToMakeValid(string s) {
-		int cnt = 0;
-		string ans = "";
-		for (auto x : s) {
-			if (x == '(') {
-				ans += x;
-				cnt++;
-			}
-			else if (x == ')') {
-				if (cnt > 0) {
-					ans += x;
-					cnt--;
-				}
-
-			}
-			else {
-				ans += x;
-			}
-		}
-		cout << ans << endl;
-		cnt = 0;
-		string res = "";
-		int n = ans.length();
-		if (n == 0)return "";
-		for (int i = n - 1; i >= 0; i--) {
-			if (ans[i] == ')') {
-				res += ans[i];
-				cnt++;
-			} else if (ans[i] == '(') {
-				if (cnt > 0) {
-					res += ans[i];
-					cnt--;
-				}
-			} else {
-				res += ans[i];
-			}
-		}
-		reverse(res.begin(), res.end());
-		return res;
-	}
+    string minRemoveToMakeValid(string s) {
+        int cnt=0;
+        string ans="";
+        for(auto x:s){
+            if(x=='('){
+                ans+=x;
+                cnt++;
+            }
+            else if(x==')'){
+                if(cnt>0){
+                    ans+=x;
+                    cnt--;
+                }
+         
+            }
+               else {
+                ans+=x;
+            }
+        }
+        cout<<ans<<endl;
+        cnt=0;
+        string res="";
+        int n=ans.length();
+        if(n==0)return "";
+        for(int i=n-1;i>=0;i--){
+            if(ans[i]==')'){
+                res+=ans[i];
+                cnt++;
+            }else if(ans[i]=='('){
+                if(cnt>0){
+                    res+=ans[i];
+                    cnt--;
+                }
+            }else{
+                res+=ans[i];
+            }
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
 };
