9434b06e55a75e77df969e1154f597b731fd4653
Pranav N <pranavbharadwaj142@gmail.com>
2022-03-28 06:37:14 +0530

    Binary search

---
+++ Searching/search-in-rotated-sorted-array-II.cpp
@@ -0,0 +1,21 @@
+// Link : https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l=0, r = nums.size()-1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target) return true;
+            if( (nums[l] == nums[mid]) && (nums[r] == nums[mid])) {l++; r--;}
+            else if(nums[l] <= nums[mid]){
+                if((nums[l] <= target) && (nums[mid] > target)) r = mid-1;
+                else l = mid + 1;
+            } else {
+                if((nums[mid] < target) && (nums[r] >= target)) l = mid + 1;
+                else r = mid-1;
+            }
+        }
+        return false;
+    }
+};
