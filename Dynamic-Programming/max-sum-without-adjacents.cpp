c4a25e3f9ebf5c885a5c1e96b76d871dfdf8dc71
Pranav N <pranavbharadwaj142@gmail.com>
2022-04-15 13:15:59 +0530

    max sum without adj

--- Dynamic-Programming/max-sum-without-adjacents.cpp
+++ Dynamic-Programming/max-sum-without-adjacents.cpp
@@ -1,6 +1,8 @@
 
 // Link : https://practice.geeksforgeeks.org/problems/max-sum-without-adjacents2430/1/
 
+// MEMO
+
 class Solution{
 public:	
 	int solve(int ind,int *arr,vector<int>&dp){
@@ -15,5 +17,39 @@ public:
 	   vector<int>dp(n,-1);
 	   
 	   return solve(n-1,arr,dp);
+	}
+};
+
+// TABULATION
+
+class Solution{
+public:	
+	int findMaxSum(int *arr, int n) {
+	   vector<int>dp(n,-1);
+	   dp[0]=arr[0];
+	   for(int i=1;i<n;i++){
+	       int pick=arr[i];if(i>1)pick+=dp[i-2];
+	       int notpick=0+dp[i-1];
+	       dp[i]=max(pick,notpick);
+	   }
+	   return dp[n-1];
+	}
+};
+
+// SPACE OPTIMIZATION
+
+class Solution{
+public:	
+	int findMaxSum(int *arr, int n) {
+       int prev1=arr[0];
+       int prev2=0;
+	   for(int i=1;i<n;i++){
+	       int pick=arr[i];if(i>1)pick+=prev2;
+	       int notpick=0+prev1;
+	       int curr=max(pick,notpick);
+	       prev2=prev1;
+	       prev1=curr;
+	   }
+	   return prev1;
 	}
 };
