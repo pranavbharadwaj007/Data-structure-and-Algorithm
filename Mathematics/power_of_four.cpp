f7bcd10a9a3f0b05cef6d43a3fdf8fd928c38fd0
Pranav N <pranavbharadwaj142@gmail.com>
2022-08-22 08:06:53 +0530

    power of 4

---
+++ Mathematics/power_of_four.cpp
@@ -0,0 +1,15 @@
+
+// Link : https://leetcode.com/problems/power-of-four/
+
+    bool isPowerOfFour(int n) {
+        if(n<=0 ){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n%4!=0){
+            return false;
+        }
+        return isPowerOfFour(n/4);
+    }
