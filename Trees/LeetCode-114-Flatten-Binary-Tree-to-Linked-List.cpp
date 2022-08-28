d1f6bc3c0add3c504c2c768691c475ae41e4b85a
Pranav N <pranavbharadwaj142@gmail.com>
2022-08-28 19:23:52 +0530

    Update LeetCode-114-Flatten-Binary-Tree-to-Linked-List.cpp

--- Trees/LeetCode-114-Flatten-Binary-Tree-to-Linked-List.cpp
+++ Trees/LeetCode-114-Flatten-Binary-Tree-to-Linked-List.cpp
@@ -18,3 +18,32 @@ public:
         
     }
 };
+
+
+// recursion
+
+class Solution
+{
+    private:
+       Node* head=NULL;Node* prev=NULL;
+    void helper(Node* root){
+        if(root==NULL)return;
+        helper(root->left);
+        if(prev==NULL)head=root;
+        else{
+            root->left=prev;
+            prev->right=root;
+            
+        }
+        prev=root;
+        helper(root->right);
+    }
+    public: 
+    //Function to convert binary tree to doubly linked list and return it.
+ 
+    Node * bToDLL(Node *root)
+    {
+        helper(root);
+        return head;
+    }
+};
