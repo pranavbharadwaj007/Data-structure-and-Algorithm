deceef2b3dab9a3bd965d62c138bfbc44801685d
Pranav N <pranavbharadwaj142@gmail.com>
2022-09-14 18:44:23 +0530

    Longest Common Substring

---
+++ Dynamic-Programming/longest_common_substring.cpp
@@ -0,0 +1,28 @@
+
+// Link : https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1
+
+int lcs(string &s1, string &s2){
+    
+    int n = s1.size();
+    int m = s2.size();
+    
+    vector<int> prev(m+1,0), cur(m+1,0);
+
+    int ans = 0;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]){
+                int val = 1 + prev[j-1];
+                cur[j] = val;
+                ans = max(ans,val);
+            }
+            else
+                cur[j] = 0;
+        }
+        prev=cur;
+    }
+    
+    return ans;
+    
+}
