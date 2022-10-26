47bc8709674787bb3504015798cbb009f2c5320c
Pranav N <pranavbharadwaj142@gmail.com>
2022-10-26 07:26:44 +0530

    Create continuous-subarray-sum.cpp

---
+++ Hashmap-Heap/continuous-subarray-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>m;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            int rem=sum%k;
+            if(rem==0 && i>0)
+            {
+                return true;
+            }
+            if(m.find(rem)==m.end())
+            {
+                m[rem]=i;
+            }
+            else if(i-m[rem]>=2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
